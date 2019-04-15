f4dd5a6d16189457bb1e62b3b0da290d07043e01
Karissa <k@karissademi.com>
2019-04-15 10:59:54 -0400

    Add foundation grid

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
+const path = require('path');
+
 module.exports = {
   lintOnSave: false,
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [path.resolve(__dirname, './src/styles/global.scss')],
+    },
+  },
 };
