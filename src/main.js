dd90f29e3b6114c80eb336e4ec5cb695e683e3cf
andy.rothwell <rothwell.andy@gmail.com>
2019-04-23 16:21:31 -0400

    adds config-mixin.js to util, adds a config.js (which can be replaced with an online file), adds imports of these to main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import configMixin from './util/config-mixin';
+import config from './config';
+
+Vue.use(configMixin, config);
+
 Vue.config.productionTip = false;
 
 new Vue({
