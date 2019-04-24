fe46ea91bfbfa642fe0a81f20fc7dfae2f86e5bd
andy.rothwell <rothwell.andy@gmail.com>
2019-04-24 09:33:39 -0400

    adds comments explaining all infrastructure added to make the $config work

--- src/config.js
+++ src/config.js
@@ -1,3 +1,10 @@
+// this is the base-config for resource-finder
+// the point of this file is that it will move outside the project
+// (so that settings we put in it can be used by other projects)
+// and be pulled in with an axios call or something
+// (we might not need to use axios with new vue async tools)
+// if that is not needed, we can move this info to main.js
+
 export default {
   map: '',
 }
--- src/main.js
+++ src/main.js
@@ -3,8 +3,25 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// importing util helpers for creating the config
+// and making it available to all components as "this.$config"
+import mergeDeep from './util/merge-deep';
 import configMixin from './util/config-mixin';
-import config from './config';
+
+// baseConfig is right now coming in from within the project
+// if we definitely need one, we can move it outside the project
+// and do an axios call to get it
+import baseConfig from './config';
+
+// any config specific to this project only can go here
+const clientConfig = {
+  // map: '',
+}
+
+// if there is a baseConfig, it is merged with the clientConfig here
+// in the parameters of mergeDeep, whichever one comes second
+// will have overwrite power over the first
+const config = mergeDeep(baseConfig, clientConfig);
 
 Vue.use(configMixin, config);
 
