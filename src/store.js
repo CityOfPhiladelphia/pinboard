8dc42524ec290436134f625bd8e5f3b5d1a01473
andy.rothwell <rothwell.andy@gmail.com>
2019-04-24 10:14:55 -0400

    adds an array of dummyMarkers and stores them in the state as "map.resourceMarkers"

--- src/store.js
+++ src/store.js
@@ -6,9 +6,64 @@ import mergeDeep from './util/merge-deep';
 
 Vue.use(Vuex);
 
+const dummyMarkers = [
+  {
+    latlng: [39.953338, -75.163471],
+    color: 'purple',
+    icon: {
+      prefix: 'fas',
+      icon: 'map-marker-alt',
+      shadow: false,
+      size: 20,
+    },
+  },
+  {
+    latlng: [39.951839, -75.160911],
+    color: 'purple',
+    icon: {
+      prefix: 'fas',
+      icon: 'map-marker-alt',
+      shadow: false,
+      size: 20,
+    },
+  },
+  {
+    latlng: [39.953205, -75.168776],
+    color: 'purple',
+    icon: {
+      prefix: 'fas',
+      icon: 'map-marker-alt',
+      shadow: false,
+      size: 20,
+    },
+  },
+  {
+    latlng: [39.949503, -75.157371],
+    color: 'purple',
+    icon: {
+      prefix: 'fas',
+      icon: 'map-marker-alt',
+      shadow: false,
+      size: 20,
+    },
+  },
+  {
+    latlng: [39.949397, -75.166866],
+    color: 'purple',
+    icon: {
+      prefix: 'fas',
+      icon: 'map-marker-alt',
+      shadow: false,
+      size: 20,
+    },
+  },
+]
+
 const rfStore = {
   state: {
-
+    map: {
+      resourceMarkers: dummyMarkers,
+    },
   },
   mutations: {
 
