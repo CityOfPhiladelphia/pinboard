e30214b8e91cae6fb9eaf2694e10bce48c7f883d
Karissa <k@karissademi.com>
2019-05-06 14:22:02 -0400

    First pass at mobile menu

---
+++ src/components/MobileMenu.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="menu-content grid-x">
+      <div class="cell">
+      <a :href="appLogoLink" class="logo flex-child-auto">
+        <img :src="appLogo" width="170" height="45" :alt="appLogoAlt">
+      </a>
+    </div>
+      menu goes here
+
+  </div>
+</template>
