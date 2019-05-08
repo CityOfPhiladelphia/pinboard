1e1cdcd31abf2d5bb858fd8bfb30abe4b0896733
Karissa <k@karissademi.com>
2019-05-08 17:28:10 -0400

    Begin refine panel styles

--- src/components/RefinePanel.vue
+++ src/components/RefinePanel.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="cell medium-cell-block-container bg-ghost-gray refine-panel">
+  <div class="cell medium-cell-block-container bg-ghost-gray refine-panel"
+  :class="{ open: refineOpen }">
     <div class="grid-x">
       <fieldset class="cell">
-        <div class="refine-title">
+        <div class="refine-title"
+          @click="expandRefine">
           <legend class="legend-title h3">{{ legendTitle }}</legend>
           <a href="#"
             @click="clearAll"
-            class="clear-all">Clear all</a>
+            class="clear-all hide-for-small-only">Clear all</a>
         </div>
         <div class="grid-x service-list">
           <div class="cell medium-6"
@@ -23,14 +25,25 @@
               </label>
             </div>
           </div>
+          <div class="filter-actions">
+            <PhilaButton
+              buttonText="Apply filters" />
+            <a href="#"
+              @click="clearAll"
+              class="">Clear all</a>
+          </div>
       </fieldset>
     </div>
   </div>
 </template>
 <script>
 import axios from 'axios'
+import PhilaButton from './PhilaButton.vue'
 
 export default {
+  components: {
+    PhilaButton,
+  },
   props: {
     legendTitle: {
       type: String,
@@ -42,6 +55,7 @@ export default {
       baseUrl: process.env.VUE_APP_BASE_URL,
       refineList: null,
       selected: [],
+      refineOpen: false,
     }
   },
   created() {
@@ -62,6 +76,9 @@ export default {
           console.log(error)
         // this.errored = true
         })
+    },
+    expandRefine() {
+      this.refineOpen = !this.refineOpen;
     },
   },
 
@@ -100,6 +117,25 @@ $refine-panel-height: 19vh;
   label {
     font-weight: normal;
   }
+  @media screen and (max-width: 39.9375em) {
+    height: 3rem;
+    padding: .5rem;
+
+    .refine-title{
+      cursor: pointer;
+
+      &::after{
+        content: '+';
+        float: right;
+        font-weight: 900;
+        font-size:1.5rem;
+      }
+    }
+  }
+}
+
+.open{
+  min-height: 100vh !important;
 }
 
 </style>
