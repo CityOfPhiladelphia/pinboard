b7e91645b28392be424293ba0c92fc82d40b1925
andy.rothwell <rothwell.andy@gmail.com>
2019-05-09 12:28:30 -0400

    add changes to MapPanel.vue to use data from store for map markers

--- src/components/MapPanel.vue
+++ src/components/MapPanel.vue
@@ -21,7 +21,7 @@
                             :zIndex="tiledLayer.zIndex"
       />
       <!-- v-if="tiledLayers.includes(key)" -->
-      <vector-marker v-for="(marker) in resourceMarkers"
+      <vector-marker v-for="(marker) in this.$data.rows"
                      :latlng="marker.latlng"
                      :key="marker.key"
                      :markerColor="marker.color"
@@ -34,6 +34,7 @@
 </template>
 
 <script>
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-unused-vars */
 import 'leaflet/dist/leaflet.css';
@@ -43,22 +44,57 @@ import Map_ from '@philly/vue-mapping/src/leaflet/Map.vue';
 
 
 export default {
+  created() {
+    // Fetch Data
+    this.getMarkerData();
+  },
   components: {
     Map_,
     EsriTiledMapLayer: () => import(/* webpackChunkName: "pvm_EsriTiledMapLayer" */'@philly/vue-mapping/src/esri-leaflet/TiledMapLayer.vue'),
     VectorMarker: () => import(/* webpackChunkName: "mbmp_pvm_VectorMarker" */'@philly/vue-mapping/src/components/VectorMarker.vue'),
   },
-  computed: {
-    resourceMarkers() {
-      return this.$store.state.map.resourceMarkers;
-    },
-    // resizeMap() {
-    //   window.onresize = function (event) {
-    //     console.log(event)
-    //     Map_.invalidateSize()
-    //   }
-    // },
+  data() {
+    const data = {
+      rows: [],
+    }
+    return data;
   },
+  methods: {
+    async getMarkerData() {
+      const response = await (this.$store.state.sources.immigrant === 'success');
+
+      const promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('done!'), 1000)
+      });
+
+      const result = await promise; // wait till the promise resolves (*)
+
+      const refineData = this.$store.state.sources.immigrant.data.rows
+      const newRows = [];
+      for (const rm of refineData) {
+        if (rm.lat) {
+          rm.latlng = [rm.lat, rm.lon];
+          rm.color = 'purple';
+          rm.icon = {
+            prefix: 'fas',
+            icon: 'map-marker-alt',
+            shadow: false,
+            size: 20,
+          }
+          newRows.push(rm);
+        }
+      }
+      this.rows = newRows;
+      return Promise.resolve();
+    },
+  },
+  //   // resizeMap() {
+  //   //   window.onresize = function (event) {
+  //   //     console.log(event)
+  //   //     Map_.invalidateSize()
+  //   //   }
+  //   // },
+  // },
 }
 </script>
 
