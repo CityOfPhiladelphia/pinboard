afb33691289f717fa5fbaa0c0213b615d0664a3e
andy.rothwell <rothwell.andy@gmail.com>
2019-09-27 11:21:46 -0400

    calls controller on handleSearchFormSubmit

--- src/components/PhilaHeader.vue
+++ src/components/PhilaHeader.vue
@@ -203,6 +203,7 @@ export default {
       // console.log('in comboSearchTriggered, query:', query, 'this.searchType:', this.searchType, '{...this.$route.query}:', { ...this.$route.query }, '{...query}:', { ...query });
       this.$router.push({ query: { ...this.$route.query, ...query }});
       this.searchString = query[this.searchType];
+      this.$controller.handleSearchFormSubmit(value, searchCategory);
     },
     clearSearchTriggered() {
       // console.log('in clearSearchTriggered, this.$route.query:', this.$route.query);
