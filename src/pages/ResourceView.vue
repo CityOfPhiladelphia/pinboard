9ed1327c0aafaad4e9539cbf63af8f209efbdc94
Andy Rothwell <Andy.Rothwell@phila.gov>
2022-12-13 15:58:59 -0500

    adds app header to resource view

--- src/pages/ResourceView.vue
+++ src/pages/ResourceView.vue
@@ -1,80 +1,79 @@
 <template>
-  <div
-    class="loc-item open"
-  >
-    <!-- :class="{ 'open': locationOpen }" -->
+  <div>
     <div
-      class="columns location-row is-mobile"
-      tabindex="0"
+      class="header-holder"
     >
-      <!-- @click="expandLocation"
-      @keypress.space.prevent
-      @keyup.space="expandLocation"
-      @keyup.enter="expandLocation" -->
-      <div class="location-title column is-11">
-        <h2
-          class="h5"
-          :aria-expanded="locationOpen"
-        >
-          {{ getSiteName(item) }}
-          <div
-            v-if="section && !i18nEnabled"
-            class="section-name"
-            :style="{ 'background-color': sectionColor }"
-            >
-            {{ sectionTitle }}
-          </div>
-          <div
-            v-if="section && i18nEnabled"
-            class="section-name"
-            :style="{ 'background-color': sectionColor }"
-            v-html="'<b>'+$t(sectionTitle)+'</b>'"
-          >
-        </div>
-
-        </h2>
-      </div>
-      <!-- <button
-        v-if="locationOpen"
-        class="print-view-button"
-        @click="openPrintView"
+      <app-header
+        :app-title="appTitle"
+        :app-subtitle="appSubTitle"
+        :app-link="appLink"
+        :is-sticky="false"
+        :branding-image="brandingImage"
+        :branding-link="brandingLink"
+        :isFluid="true"
       >
-        Print View
-      </button>
-      <div class="location-icon column is-1">
-        <font-awesome-icon
-          v-if="!locationOpen"
-          :icon="[plusIconWeight, 'plus']"
-          class="plus-icon"
-        />
-        <font-awesome-icon
-          v-if="locationOpen"
-          :icon="[plusIconWeight, 'minus']"
-        />
-      </div> -->
+        <mobile-nav
+          slot="mobile-nav"
+          :links="footerLinks"
+        >
+        </mobile-nav>
+
+        <lang-selector
+          slot="lang-selector-nav"
+          v-if="i18nEnabled"
+          :languages="i18nLanguages"
+        >
+        </lang-selector>
+
+      </app-header>
     </div>
 
     <div
-      class="component-holder"
+      class="loc-item open"
     >
+      <div
+        class="columns location-row is-mobile"
+        tabindex="0"
+      >
+        <div class="location-title column is-11">
+          <h2
+            class="h5"
+            :aria-expanded="locationOpen"
+          >
+            {{ getSiteName(item) }}
+            <div
+              v-if="section && !i18nEnabled"
+              class="section-name"
+              :style="{ 'background-color': sectionColor }"
+              >
+              {{ sectionTitle }}
+            </div>
+            <div
+              v-if="section && i18nEnabled"
+              class="section-name"
+              :style="{ 'background-color': sectionColor }"
+              v-html="'<b>'+$t(sectionTitle)+'</b>'"
+            >
+          </div>
 
-      <component
-        :is="'expandCollapseContent'"
-        v-if="$config.customComps && Object.keys($config.customComps).includes('expandCollapseContent') && selectedResources.includes(item._featureId)"
-        :item="item"
-      />
+          </h2>
+        </div>
+      </div>
+
+      <div class="component-holder">
+        <component
+          :is="'expandCollapseContent'"
+          v-if="$config.customComps && Object.keys($config.customComps).includes('expandCollapseContent') && selectedResources.includes(item._featureId)"
+          :item="item"
+        />
+      </div>
+
     </div>
-
-      <!-- :is-map-visible="isMapVisible" -->
-    <!-- <div
-      :class="locationClass"
-    >
-      <slot />
-    </div> -->
-
   </div>
 
 </template>
+
+
 <script>
 
 import SharedFunctions from '@/components/mixins/SharedFunctions.vue';
@@ -84,53 +83,71 @@ import { findIconDefinition } from '@fortawesome/fontawesome-svg-core';
 // import all fontawesome icons included in phila-vue-mapping
 import * as faMapping from '@phila/vue-mapping/src/fa';
 
+import {
+  AppHeader,
+  MobileNav,
+  AppFooter,
+  InputForm,
+  Textbox,
+  Checkbox,
+  LangSelector,
+} from '@phila/phila-ui';
+
 export default {
-  props: {
-    isMapVisible: {
-      type: Boolean,
-      default: true,
-    },
-    // item: {
-    //   type: Object,
-    // },
+  components: {
+    AppHeader,
+    MobileNav,
+    AppFooter,
+    InputForm,
+    Textbox,
+    Checkbox,
+    LangSelector,
   },
   data() {
     return {
       resource: null,
-      // item: null,
       locationOpen: false,
     };
   },
   mixins: [ SharedFunctions ],
   computed: {
-    locationClass() {
+    appTitle() {
       let value;
-      if (this.locationOpen && this.isMobile) {
-        value = 'location-content-mobile location-open';
-      } else if (this.locationOpen) {
-        value = 'location-content location-open';
-      } else if (this.isMobile) {
-        value = 'location-content-mobile';
+      if (this.$config.app.title) {
+        value = this.$config.app.title;
+      } else if (this.i18nEnabled) {
+        value = this.$i18n.messages[this.i18nLocale].app.title;
+      }
+      return value;
+    },
+    appSubTitle() {
+      let value;
+      if (this.$config.app.subtitle) {
+        value = this.$config.app.subtitle;
+      } else if (this.i18nEnabled) {
+        value = this.$i18n.messages[this.i18nLocale].app.subtitle;
+      }
+      return value;
+    },
+    i18nLocale() {
+      return this.$i18n.locale;
+    },
+    i18nLanguages() {
+      let values = [];
+      if (this.$config.i18n.languages) {
+        console.log('in if, this.$config.i18n.languages');
+        values = this.$config.i18n.languages;
       } else {
-        value = 'location-content';
+        for (let key of Object.keys(this.$i18n.messages)) {
+          let value = {};
+          // console.log('in loop, key:', key, 'this.$i18n.locale:', this.$i18n.locale, 'this.$i18n.messages[key]:', this.$i18n.messages[key]);
+          value.language = key;
+          value.title = this.$i18n.messages[key].language;
+          values.push(value);
+        }
       }
-      return value;
-    },
-    plusIconWeight() {
-      let value = 'fas';
-      let regularExists = findIconDefinition({ prefix: 'far', iconName: 'plus' });
-      // console.log('expandCollapse.vue computed, library:', library, 'regularExists:', regularExists);
-      if (regularExists) {
-        value = 'far';
-      }
-      return value;
-    },
-    showLabels() {
-      let value = false;
-      if (this.$config.refine.showLabels) {
-        value = true;
-      }
-      return value;
+      console.log('end of i18nLanguages, values:', values);
+      return values;
     },
     i18nEnabled() {
       let value;
@@ -140,43 +157,6 @@ export default {
         value = false;
       }
       return value;
-    },
-    subsections() {
-      return this.$config.subsections || {};
-    },
-    section() {
-      let section;
-      if (Object.keys(this.subsections).length) {
-        section = this.subsections[this.$props.item.attributes['CATEGORY']];
-      } else if (this.$config.sections) {
-        section = this.$props.item.site_type;
-      }
-      return section;
-    },
-    sectionItem() {
-      let sectionItem = {};
-      if (Object.keys(this.subsections).length) {
-        sectionItem = this.$config.sections[this.section];
-      }
-      return sectionItem;
-    },
-    sectionTitle() {
-      let sectionTitle;
-      if (Object.keys(this.subsections).length) {
-        sectionTitle = this.$config.sections[this.section].titleSingular;
-      } else if (this.$config.sections) {
-        sectionTitle = this.$props.item.site_type;
-      }
-      return sectionTitle;
-    },
-    sectionColor() {
-      let sectionColor;
-      if (Object.keys(this.subsections).length) {
-        sectionColor = this.$config.sections[this.section].color;
-      } else if (this.$config.sections) {
-        sectionColor = this.$config.sections[this.section].color;
-      }
-      return sectionColor;
     },
     selectedResources() {
       return this.$store.state.selectedResources;
@@ -189,7 +169,6 @@ export default {
       console.log('computed item test:', test, 'this.resource:', this.resource);
       let value;
       if (this.$store.state.sources.primaryCareSites.data) {
-        // value = this.$store.state.sources.primaryCareSites.data.features.filter(features => features._featureId = this.resource);
         value = this.$store.state.sources.primaryCareSites.data.features.filter(features => {
           // console.log('inside filter function, features:', features, 'this.resource:', this.resource);
           return features._featureId == this.resource;
@@ -197,39 +176,6 @@ export default {
       }
       console.log('computed item, value:', value);
       return value;
-      // return this.$store.state.sources.primaryCareSites.data.features.filter(function(_featureId) {
-      //   console.log('inside filter function, _featureId:', _featureId, 'this.resource:', this.resource);
-      //   return _featureId = this.resource;
-      // });
-    },
-  },
-  watch: {
-    // selectedResources(nextSelectedResources) {
-    //   // console.log('watch selectedResources is running');
-    //   if (this.locationOpen || nextSelectedResources.includes(this.$props.item._featureId)) {
-    //     if (this.locationOpen === false) {
-    //       this.openLocation();
-    //     } else if (this.locationOpen && !nextSelectedResources.includes(this.$props.item._featureId)) {
-    //       this.locationOpen = false;
-    //     }
-    //   } else {
-    //     this.locationOpen = false;
-    //   }
-    // },
-    isMapVisible(nextIsMapVisible) {
-      // console.log('ExpandCollapse watch isMapVisible');
-      if (!nextIsMapVisible) {
-        if (this.latestSelectedResourceFromMap) {
-          // console.log('ExpandCollapse is reporting map is invisible and there is a this.latestSelectedResourceFromMap:', this.latestSelectedResourceFromMap);
-          if (this.latestSelectedResourceFromMap === this.item._featureId) {
-            const el = this.$el;
-            const visible = this.isElementInViewport(el);
-            if (!visible) {
-              el.scrollIntoView();
-            }
-          }
-        }
-      }
     },
   },
   mounted() {
@@ -238,32 +184,8 @@ export default {
     }
 
     this.resource = this.$route.params.resource;
-    // let test = this.$store.state.sources.primaryCareSites.data;
-    // console.log('mounted test:', test);
-    // this.item = this.$store.state.sources.primaryCareSites.data.features.filter(_featureId => _featureId = this.resource);
     let selectedResources = [ this.resource ];
     this.$store.commit('setSelectedResources', selectedResources);
-
-    // console.log('ExpColl mounted:', this.evaluateSlot(this.slots.siteName));
-    // if (this.selectedResources.includes(this.item._featureId)) {
-    //   this.openLocation();
-    // }
-
-    // window.addEventListener('keydown', (e) => {
-    //   console.log('keydown is running, e', e);
-    //   if (e.keyCode === 32 && e.target === document.body) {
-    //     e.preventDefault();
-    //   }
-    // });
-
-    // let divButton = document.querySelector('#refine-top');
-    // divButton.addEventListener('keypress', activate.bind(this));
-    // function activate(e) {
-    //   console.log('activate, e:', e, 'e.path[0]:', e.path[0]);
-    //   if (e.type === 'keypress' && [ 13, 32 ].includes(e.keyCode) && e.srcElement.id == 'refine-top') {
-    //     this.expandRefine();
-    //   }
-    // };
   },
   methods: {
     openPrintView(e) {
@@ -271,86 +193,57 @@ export default {
       console.log('openPrintView is running, e:', e, 'this.$props.item._featureId:', this.$props.item._featureId);
       window.open();
     },
-    openLocation() {
-      this.locationOpen = true;
-      const el = this.$el;
-      // console.log('ExpandCollapse openLocation is running, el:', el);
-      let visible = this.isElementInViewport(el);
-      // console.log('visible 1:', visible)
-      if (!visible) {
-        el.scrollIntoView();
-      }
-    },
-    isElementInViewport(el) {
-      const rect = el.getBoundingClientRect();
-      // console.log('bounding box', rect);
-      const visibility = {
-        // TODO the 108 below is account for the combined height of the
-        // app header and address header. this is not a good long-term
-        // solution - instead, use the `bottom` value of the address header's
-        // bounding rect. however, this should only fire on small devices,
-        // which would require again hard-coding screen breakpoints from
-        // standards or some other magic, which might not a huge
-        // improvement in terms of decoupling logic and presentation. hmm.
-        top: rect.top >= 108,
-        left: rect.left >= 0,
-        bottom: rect.bottom <= (window.innerHeight || document.documentElement.clientHeight),
-        right: rect.right <= (window.innerWidth || document.documentElement.clientWidth),
-      };
-
-      // console.log('visibility', visibility);
-
-      // return if all sides are visible
-      return Object.values(visibility).every(val => val);
-    },
-    // expandLocation() {
-    //   let siteName = this.getSiteName(this.$props.item);
-    //   console.log('ExpandCollapse expandLocation is starting, siteName:', siteName);
-    //   this.locationOpen = !this.locationOpen;
-    //   const selectedResource = this.$props.item._featureId;
-    //   // const selectedResources = [ ...this.selectedResources ];
-    //   const selectedResources = [];
-    //   let latestSelectedResourceFromExpand = null;
-    //   if (this.locationOpen) {
-    //     selectedResources.push(selectedResource);
-    //     latestSelectedResourceFromExpand = selectedResource;
-    //     this.$gtag.event('list-click', {
-    //       'event_category': this.$store.state.gtag.category,
-    //       'event_label': siteName,
-    //     });
-    //   } else {
-    //     selectedResources.splice(selectedResources.indexOf(selectedResource), 1);
-    //   }
-    //   // this.locationOpen ? selectedResources.push(selectedResource) : selectedResources.splice(selectedResources.indexOf(selectedResource), 1);
-    //   // console.log('ExpandCollapse expandLocation after selectedResources is defined');
-
-    //   this.$store.commit('setSelectedResources', selectedResources);
-    //   this.$store.commit('setLatestSelectedResourceFromExpand', latestSelectedResourceFromExpand);
-    // },
-    makeID( itemTitle ){
-      // console.log('itemTitle:', itemTitle);
-      let value;
-      if (itemTitle) {
-        value = itemTitle.replace(/\s+/g, '-').toLowerCase();
-      } else {
-        value = '';
-      }
-      return value;
-    },
   },
 };
 </script>
 <style lang="scss">
 @import "../assets/scss/main.scss";
 
+#app-header {
+
+  .trusted-site-hidden {
+    display: none;
+  }
+
+  #nav-wrap {
+    height: 80px;
+    line-height: 80px;
+
+
+    #main-nav {
+
+      .columns {
+        height: 80px;
+        .column {
+          height: 80px;
+          padding-top: 0rem;
+        }
+
+        .title-col {
+          padding-top: 1rem !important;
+          padding-bottom: 0rem;
+        }
+      }
+    }
+
+  }
+  .container {
+    padding-left: 16px !important;
+    padding-right: 16px !important;
+  }
+  h2 {
+    font-weight: 100;
+  }
+
+  // .title-col {
+  //   padding-top: 1rem;
+  // }
+
+}
+
 .loc-item {
   position: relative;
-  // border-bottom: 1px solid black;
   height:100%;
-
-  // &:hover::after {
-  //   color: white;
-  // }
 
   &:hover {
     .plus-icon {
@@ -374,6 +267,8 @@ export default {
   .location-title {
     padding-top: 1rem;
     padding-bottom: 1rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
   }
 
   .location-icon {
@@ -389,45 +284,27 @@ export default {
     padding-left: 14px;
     padding-right: 14px;
     font-size: 12px;
-    // border-style: solid;
-    // border-width: 1px;
     display: inline-block;
     color: white;
   }
 
-  // &::after{
-  //   position: absolute;
-  //   right: 1rem;
-  //   top: 1rem;
-  //   content: '+';
-  //   font-weight: 300;
-  //   font-size:1.5rem;
-  //   z-index: 100;
-  //   color: $ben-franklin-blue-dark;
-  //   pointer-events: none;
-  // }
   &.open{
     .location-row {
       color:white;
-      background-color: $ben-franklin-blue-dark;
-      // font-weight: 900;
+      // background-color: $ben-franklin-blue-dark;
+      background-color: #2176d2;;
     }
 
-    // .location-title {
     h2 {
       font-weight: 900 !important;
     }
-    // &::after{
-    //   content: '–';
-    //   color:white;
-    // }
   }
+
   .location-content{
     overflow: hidden;
     height:0;
 
     &.location-open{
-      // padding: 1rem;
       padding-top: 1rem;
       padding-bottom: 1rem;
       padding-right: 0px;
