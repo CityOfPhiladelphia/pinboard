b153833742f8afe64c82401ccbf8c12650f24fce
Andy Rothwell <Andy.Rothwell@phila.gov>
2023-04-13 15:00:31 -0400

    changes routing in App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,8 @@ export default {
   name: 'App',
   created () {
     console.log('route', this.$route.path)
-    this.$router.replace(this.$route.query.redirect || '/')
+    // this.$router.replace(this.$route.query.redirect || '/')
+    this.$router.push({ name: 'home'  });
   }
 };
 
