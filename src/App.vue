8a1d711e338705c8f638f31a5b11c791ad2e7253
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-05-23 07:31:40 -0400

    adds qs package, makes call to ago for token

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,43 @@ export default {
   name: 'App',
   created () {
     console.log('route', this.$route.path)
+
     // this.$router.replace(this.$route.query.redirect || '/')
     // this.$router.push({ name: 'home'  });
+  
+
+    if (this.$config.agoTokenNeeded) {
+
+      let data = qs.stringify({
+        'f': 'json',
+        'username': process.env.VUE_APP_AGO_USERNAME,
+        'password': process.env.VUE_APP_AGO_PASSWORD,
+        'referer': 'https://www.mydomain.com' 
+      });
+
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://www.arcgis.com/sharing/rest/generateToken',
+        headers: { 
+          'Content-Type': 'application/x-www-form-urlencoded', 
+          // 'Authorization': 'Basic Og=='
+        },
+        data : data
+      };
+
+      axios.request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        this.$store.commit('setAgoToken', response.data.token);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    }
+
+
   }
 };
 
