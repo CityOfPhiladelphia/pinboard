cea1a5825764ff1d8779fd7f9b94835b097925f1
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2024-05-29 11:47:09 -0400

    uses @phila/vue-datafetch commit that adds agoTokenClient

--- src/App.vue
+++ src/App.vue
@@ -16,36 +16,36 @@ export default {
     // this.$router.push({ name: 'home'  });
   
 
-    if (this.$config.agoTokenNeeded) {
+    // if (this.$config.agoTokenNeeded) {
 
-      let data = qs.stringify({
-        'f': 'json',
-        'username': process.env.VUE_APP_AGO_USERNAME,
-        'password': process.env.VUE_APP_AGO_PASSWORD,
-        'referer': 'https://www.mydomain.com' 
-      });
+    //   let data = qs.stringify({
+    //     'f': 'json',
+    //     'username': process.env.VUE_APP_AGO_USERNAME,
+    //     'password': process.env.VUE_APP_AGO_PASSWORD,
+    //     'referer': 'https://www.mydomain.com' 
+    //   });
 
-      let config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: 'https://www.arcgis.com/sharing/rest/generateToken',
-        headers: { 
-          'Content-Type': 'application/x-www-form-urlencoded', 
-          // 'Authorization': 'Basic Og=='
-        },
-        data : data
-      };
+    //   let config = {
+    //     method: 'post',
+    //     maxBodyLength: Infinity,
+    //     url: 'https://www.arcgis.com/sharing/rest/generateToken',
+    //     headers: { 
+    //       'Content-Type': 'application/x-www-form-urlencoded', 
+    //       // 'Authorization': 'Basic Og=='
+    //     },
+    //     data : data
+    //   };
 
-      axios.request(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-        this.$store.commit('setAgoToken', response.data.token);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    //   axios.request(config)
+    //   .then((response) => {
+    //     console.log(JSON.stringify(response.data));
+    //     this.$store.commit('setAgoToken', response.data.token);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
 
-    }
+    // }
 
 
   }
